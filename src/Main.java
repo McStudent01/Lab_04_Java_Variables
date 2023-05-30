4ccb73d04a726b75a05d2fc282042199729becad
McStudent01 <mcmillcy@mail.uc.edu>
2023-05-30 18:55:42 -0500

    First update

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,53 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        int intOperandA = 1;
+        int intOperandB = 21;
+        int intSum = 5;
+        int intProduct = 87;
+        int intDifference = 14;
+        int intQuotient = 9;
+        int intModulo = 2;
+        double doubleOperandA = 3.50;
+        double doubleOperandB = 1.75;
+        double doubleSum = 0.05;
+        double doubleProduct = 8.87;
+        double doubleDifference = 2.07;
+        double doubleQuotient = 9.50;
+        double doubleModulo = 55.6;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " + " + intOperandB + " is " + intSum);
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product using ints of " + intOperandA + " * " + intOperandB + " is " + intProduct);
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference using ints of " + intOperandA + " - " + intOperandB + " is " + intDifference);
+        intQuotient = intOperandB / intOperandA;
+        System.out.println("The division using ints of " + intOperandB + " / " + intOperandA + " is " + intQuotient);
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The remainder using ints of " + intOperandA + " % " + intOperandB + " is " + intModulo);
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum using ints of " + doubleOperandA + " + " + doubleOperandB + " is " + doubleSum);
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product using ints of " + doubleOperandA + " * " + doubleOperandB + " is " + doubleProduct);
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference using ints of " + doubleOperandA + " - " + doubleOperandB + " is " + doubleDifference);
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The division using ints of " + doubleOperandA + " / " + doubleOperandB + " is " + doubleQuotient);
+        doubleModulo = doubleOperandA % doubleOperandB;
+        System.out.println("The remainder using ints of " + doubleOperandA + " % " + doubleOperandB + " is " + doubleModulo);
+
+        int kidsAtHome = 3;
+        boolean isItRaining = false;
+        double priceGas = 2.98;
+        int favNum = 15;
+        int shoeSize = 9;
+        int birthMonth = 2;
+        String fullName = "CaseyMcMillen";
+
+
+
+
     }
 }
